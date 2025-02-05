862acf7762a52b0657877bb9ac542fc49229aafe
rahul <rahul.bankoti@propvivo.com>
2025-02-06 01:15:23 +0530

    Clear Git cache

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,12 +13,11 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": ["off"],
+      '@typescript-eslint/no-explicit-any': 'off', 
+      '@typescript-eslint/no-unused-vars':'off',
       "react/prop-types": ["off"],
       "react/react-in-jsx-scope": ["off"],
-      "react-hooks/rules-of-hooks": "off", 
-      "no-var": "error",
+      "no-var": "error"
     },
   },
 ];
